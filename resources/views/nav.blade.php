113d3c47f32aaad94c516c1221f13ed0ba4237a7
kimyw2353 <kimyw2353@naver.com>
2022-11-29 14:49:08 +0900

    레이아웃 블레이드 생성

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,6 @@
+<ul style="list-style: none;">
+    <li><a href="/">Home</a></li>
+    <li><a href="/post">PostList</a></li>
+    <li><a href="/login">Login</a></li>
+    <li><a href="/register">Register</a></li>
+</ul>
