d0b411dc0c9ed3bd8c1bfe467e75f9b763cc8572
kimyw2353 <kimyw2353@naver.com>
2022-11-30 12:18:13 +0900

    UserList 추가

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -2,5 +2,5 @@
     <li><a href="/">Home</a></li>
     <li><a href="/post">PostList</a></li>
     <li><a href="/login">Login</a></li>
-    <li><a href="/register">Register</a></li>
+    <li><a href="/users">User List</a></li>
 </ul>
