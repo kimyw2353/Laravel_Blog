d7418fd30827ce254e493097b34aee77ecb9c611
kimyw2353 <kimyw2353@naver.com>
2022-12-01 15:51:20 +0900

    로그아웃버튼 추가

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,11 @@
+@php use Illuminate\Support\Facades\Session; @endphp
 <ul style="list-style: none;">
     <li><a href="/">Home</a></li>
     <li><a href="/post">PostList</a></li>
-    <li><a href="/login">Login</a></li>
+    @if( !Session::has('user') )
+        <li><a href="/login">Login</a></li>
+    @elseif(Session::has('user'))
+        <li><a href="/logout">Logout</a></li>
+    @endif
     <li><a href="/users">User List</a></li>
 </ul>
