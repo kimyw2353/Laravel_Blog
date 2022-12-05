23018ea7605f1f049040f258e5a253330a16e0d2
kimyw2353 <kimyw2353@naver.com>
2022-12-05 10:35:27 +0900

    Authenticate 미들웨어 수정

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,16 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Session;
+use Closure;
 
-class Authenticate extends Middleware
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
+
+    public function handle($request, Closure $next){
+        if (! Session::has('user')){
+            return redirect('/login');
         }
+        return $next($request);
     }
 }
